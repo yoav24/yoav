ff02dbd993308d4ed8678381874f5fc3c0572a9c
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-07-28 12:00:35 +0300

    function

---
+++ 2 - JavaScript/1 - Basic/index.js
@@ -0,0 +1,31 @@
+// NaN = Not a Number
+
+// alert("hello") //only ok
+// let userAns = +prompt("Hello")
+// console.log(userAns)
+// let userAnsCon = confirm('tralalala')
+// console.log(userAnsCon)
+
+// function helloFun() {
+//     let userAns = prompt("Hello")
+//     userAns = userAns + userAns
+//     console.log(userAns)
+// }
+
+// 4+2=6
+// 6=4+x // 6-4=x
+// //2=x
+
+function func(a, b) { //regular function
+    return a + b
+}
+
+(a, b) => { //anonymous function
+    return a + b
+}
+
+const funcArrow = (a, b) => { // arrow function
+    return a + b
+}
+
+
