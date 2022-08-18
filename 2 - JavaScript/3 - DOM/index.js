8dc9984cc56edd70ea93183d5f2195bcdc2fa37c
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-08-18 11:38:20 +0300

    classs work

---
+++ 2 - JavaScript/3 - DOM/index.js
@@ -0,0 +1,36 @@
+
+// const root = document.querySelector('#root')
+// console.log(root)
+
+// console.log(root.innerText)
+// console.log(root.innerHTML)
+// root.innerText = "<p>Yoav</p>"
+// root.innerHTML = "<p>Yoav2</p>"
+
+// root.style.background = "blue"
+// root.style.fontSize = "1.5rem"
+// root.style.color = "white"
+
+// root.classList.add("fromJS")
+
+// function handleGetText() {
+//     console.log(root.innerText)
+// }
+
+// root.onclick = handleGetText
+
+//---------------------------------
+// const rootALL = document.querySelectorAll('.root')
+// console.log(rootALL)
+
+
+// // console.log(rootALL.innerText)
+// rootALL.forEach((el) => {
+//     console.log(el.innerText)
+//     el.style.color = "red"
+// })
+
+//-------------------------------
+
+// Create square with red background
+// Create function that change the background color - onclick
