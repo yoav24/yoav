30cf6394048dc850da7bb99c8c5e3f7074c142a9
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-09-14 18:43:22 +0300

    14.09

---
+++ 2 - JavaScript/6 - AddEventListener.part2/index.js
@@ -0,0 +1,35 @@
+// const btnDbl = document.getElementById('btnDBLClick')
+// const root = document.querySelector('.root')
+
+// btnDbl.addEventListener('dblclick', () => {
+//     alert('You click on me duble')
+// })
+
+// window.addEventListener('keyup', (event) => {
+//     if (event.code === "ArrowUp") {
+//         root.innerHTML += "<p>&uarr;</p>"
+//     } else if (event.code == 'ArrowDown') {
+//         root.innerHTML += "<p>&darr;</p>"
+//     } else if (event.code == 'ArrowLeft') {
+//         root.innerHTML += "<p>&rarr;</p>"
+//     } else if (event.code == 'ArrowRight') {
+//         root.innerHTML += "<p>&larr;</p>"
+//     } else {
+//         root.innerHTML += `<p>${event.code}</p>`
+//     }
+// })
+
+////////////////////////////////////////////
+// const mouseDiv = document.getElementById('mouse')
+
+// mouseDiv.addEventListener('mouseenter', () => {
+//     mouseDiv.style.backgroundColor = "yellow"
+// })
+
+// mouseDiv.addEventListener('mouseout', () => {
+//     mouseDiv.style.backgroundColor = "cadetblue"
+
+//     setTimeout(() => {
+//         mouseDiv.style.backgroundColor = "red"
+//     }, 5000)
+// })
