534b52ceebbaff7c9e6d5d17f9ae8a2a3c560550
yoav24 <103183940+yoav24@users.noreply.github.com>
2022-09-27 22:43:19 +0300

    mouse enter and out and change height by arrows

---
+++ home work js/mouse-enter out/index.js
@@ -0,0 +1,20 @@
+const button = document.querySelector(".button")
+const head = document.querySelector(".head")
+
+
+button.addEventListener('mouseenter',() =>{
+   button.style.backgroundColor =  "hsl(158, 55%, 37%)"
+   button.innerHTML = `<i class="fa-solid fa-cart-plus"></i> add to cart `
+})
+button.addEventListener('mouseout',() =>{
+   button.style.backgroundColor =  "hsl(158, 36%, 37%)"
+   button.innerHTML = `<i class="fa-solid fa-cart-shopping"></i> add to cart `
+})
+window.addEventListener('keyup',(event) =>{
+   if(event.code === "ArrowUp"){
+    head.style.height-="2.5%"
+   }
+   else if(event.code ==="ArrowDown"){
+    head.style.height+="2.5%"
+   }
+})
