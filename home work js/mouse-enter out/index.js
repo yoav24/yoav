e73aed7854838a375695d15ee90e43f724ec53e6
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-10-12 17:54:48 +0300

    card

--- home work js/mouse-enter out/index.js
+++ home work js/mouse-enter out/index.js
@@ -2,19 +2,19 @@ const button = document.querySelector(".button")
 const head = document.querySelector(".head")
 
 
-button.addEventListener('mouseenter',() =>{
-   button.style.backgroundColor =  "hsl(158, 55%, 37%)"
+button.addEventListener('mouseenter', () => {
+   button.style.backgroundColor = "hsl(158, 55%, 37%)"
    button.innerHTML = `<i class="fa-solid fa-cart-plus"></i> add to cart `
 })
-button.addEventListener('mouseout',() =>{
-   button.style.backgroundColor =  "hsl(158, 36%, 37%)"
+button.addEventListener('mouseout', () => {
+   button.style.backgroundColor = "hsl(158, 36%, 37%)"
    button.innerHTML = `<i class="fa-solid fa-cart-shopping"></i> add to cart `
 })
-window.addEventListener('keyup',(event) =>{
-   if(event.code === "ArrowUp"){
-    head.style.bottom+="2.5%"
+window.addEventListener('keyup', (event) => {
+   if (event.code === "ArrowUp") {
+      head.style.bottom += "2.5%"
    }
-   else if(event.code ==="ArrowDown"){
-    head.style.top="2.5%"
+   else if (event.code === "ArrowDown") {
+      head.style.top = "2.5%"
    }
 })
